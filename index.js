f7c05970a6b03ef772cdd60266b2b35e81f8ab97
dedsou <156522413+dedsou@users.noreply.github.com>
2024-06-19 16:23:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import { Client } from "discord.js-selfbot-v13";
-const { Streamer } = require('@dank074/discord-video-stream');
 
-const streamer = new Streamer(new Client());
-await streamer.client.login(process.env.TTOKEN);
+const { Streamer } = await import('@dank074/discord-video-stream');
+
+const client = new Client();
+const streamer = new Streamer(client);
+
+await client.login(process.env.TOKEN);
 await streamer.joinVoice(1205265436862578780, 1251029660729348156);
 
 const udp = await streamer.createStream();
 udp.mediaConnection.setSpeaking(true);
 udp.mediaConnection.setVideoStatus(true);
+
 try {
     const res = await streamLivestreamVideo("http://live.adultiptv.net/russian.m3u8", udp);
 
